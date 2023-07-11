c9833dc3f4f5bd8afcbe7a8c926dd4bfd2bf984e
tomaskurtenperez <117606826+tomaskurtenperez@users.noreply.github.com>
2023-07-11 17:41:04 -0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,7 @@
+
+const navbarMenu = document.querySelector("#navigation #navbar-menu");
+const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
+
+hamburgerMenu.addEventListener('click', function() {
+    navbarMenu.classList.toggle("open");
+});
