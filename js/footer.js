d555fd3bc38246e37581455fc07aab96fb2b7795
tomaskurten <tomas.kurten@bue.edu.ar>
2023-07-13 19:27:29 -0300

    cambio general

---
+++ js/footer.js
@@ -0,0 +1,37 @@
+document.write(`
+
+<footer id="footer">
+<div class="container">
+    
+    <div class="right-section">
+        <div class="lists">
+            <ul>
+                <li><h2>INFORMACIÓN</h2></li>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="about.html">Nosotros</a></li>
+                <li><a href="nosotros.html">Equipo</a></li>
+               
+            </ul>
+            <ul>
+                <li><h2>EN ARTICULACIÓN CON</h2></li>
+                <li><a title="ONG" href="https://www.aedin.org/
+                    " target="_blank">AEDIN</a></li>
+                <li><a title="ONG" href="http://labs.ing.unlp.edu.ar/electrotecnia/unitec/" target="_blank">UNITEC</a>
+                <li><a href="https://proyectoyaguarete.org.ar/">Proyecto Yaguareté</a></li>
+            </ul>
+            <ul>
+                <li><h2>CONTACTOS</h2></li>
+                <li><a title="Twitter" href="https://www.linkedin.com/in/tom%C3%A1s-k%C3%BCrten-p%C3%A9rez-588b7276/" target="_blank">ML Engineer</a></li>
+                <li><a title="instagram" href="jeronimo.sobral.delia@gmail.com" target="_blank">Desarrolo de Apps</a>
+                <li><a href="https://www.linkedin.com/in/emilia-beyreuther-459110256/">Medicina</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<div class="credit">
+    <span class="copyright">ArgVision Copyright 2023</span></div>
+</footer>
+
+`);
---
+++ js/nav.js
@@ -0,0 +1,21 @@
+document.write(`
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container-fluid">
+  
+    <a class="navbar-brand" href="index.html"><img src="images/LOGO.png" alt="logo" style="width: 15%;"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+            
+            <a class="nav-link" href="nosotros.html">Nosotros</a>
+            <a class="nav-link" href="about.html">Proyectos</a>
+            <a class="nav-link" href="index.html">Exposiciones</a>
+        </div>
+    </div>
+</div>
+</nav>
+
+`);
