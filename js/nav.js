cdc9bcd387fe5e2ce50e1b91f1ef633ef570361b
tomaskurten <tomas.kurten@bue.edu.ar>
2023-07-14 18:10:50 -0300

    favicon

--- js/nav.js
+++ js/nav.js
@@ -4,7 +4,7 @@ document.write(`
 
 <div class="container-fluid">
   
-    <a class="navbar-brand" href="index.html"><img src="images/LOGO.png" alt="logo" style="width: 15%;"></a>
+    <a class="navbar-brand" href="index.html"><img src="images/logobienarg.png" alt="logo" style="width: 15%;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
